81fbc675f023f4d86cfd5c1d237d18c5ee27f6c6
Lite98project <bilguunee990@gmail.com>
2025-01-31 20:23:40 +0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "T41Ys30To[E-1aPAt`{3":
+            case "mouseClicked":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . . f 1 f f f . . . . . 
+. . . . f f f 1 1 1 1 f . . . . 
+. . . . f 1 f 1 1 1 1 f . . . . 
+. . . . . f 1 1 1 1 1 f . . . . 
+. . . . . . f 1 1 1 1 f . . . . 
+. . . . . . . f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "g-fZ`kxNnxNl2,RD_0rz":
+            case "mouseNotClicked":return img`
+f f . . . . . . . . . . . . . . 
+f 1 f . . . . . . . . . . . . . 
+f 1 1 f . . . . . . . . . . . . 
+f 1 1 1 f . . . . . . . . . . . 
+f 1 1 1 1 f . . . . . . . . . . 
+f 1 1 1 1 1 f . . . . . . . . . 
+f 1 1 1 1 1 1 f . . . . . . . . 
+f 1 1 f f f f f f . . . . . . . 
+f 1 f . . . . . . . . . . . . . 
+f f . . . . . . . . . . . . . . 
+f . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
